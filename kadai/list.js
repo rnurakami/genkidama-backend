479d4c6919195a378f0381c1c52754da2b98fd03
河野愛樹 <kouno_yoshiki@r.recruit.co.jp>
2019-03-17 04:13:32 +0900

    nest地獄からの脱出

--- kadai/list.js
+++ kadai/list.js
@@ -5,28 +5,54 @@ const AWS = require('aws-sdk');
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 const s3 = new AWS.S3();
 
-module.exports.list = (event, context, callback) => {
+module.exports.list = async (event, context, callback) => {
 
     const params = {
         TableName: process.env.DYNAMODB_TABLE,
 	};
-	console.log(params)
-	
-	dynamodb.scan(params, (error, result) => {
-		if(error) {
-            console.error(error);
-            callback(null, {
-                statusCode: error.statusCode || 501,
-                headers: { 'Content-Type': 'text/plain' },
-                body: 'Couldn\'t fetch the items.',
-            });
-            return;
-		}
+    console.log(params)
+    
+    try {
+        const resultKadais = await dynamodb.scan(params, (error, result) => {}).promise();
+        console.log("resultKadais")
+        console.log(resultKadais)
+
+        for (let index = 0; index < resultKadais.Items.length; index++) {
+            var params_usertable = {
+                TableName : process.env.DYNAMODB_TABLE_USER,
+                Key: {
+                    userId: resultKadais.Items[index].owner,
+                }
+            };
+
+            const result_user = await dynamodb.get(params_usertable, (error, result) => {}).promise();
+            console.log("user")
+            console.log(result_user)
+
+            resultKadais.Items[index].owner = {
+                userId: result_user.Item.userId,
+                userName: result_user.Item.userName,
+                picture: result_user.Item.picture,
+            }
+            console.log("new-kadai");
+            console.log(resultKadais.Items[index]);
+        };
 
         const response = {
             statusCode: 200,
-            body: JSON.stringify(result.Items),
+            body: JSON.stringify(resultKadais.Items),
         };
-        callback(null, response);
-	});
+
+        return response
+
+    } catch(error) {
+        console.error(error);
+        response = {
+            statusCode: error.statusCode || 501,
+            headers: { 'Content-Type': 'text/plain' },
+            body: 'Couldn\'t fetch the todos item.',
+        };
+        return response;
+    }
+
 };
