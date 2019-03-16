3566a83cb204e43fd3f20eaf05ec4e94014c3f34
河野愛樹 <kouno_yoshiki@r.recruit.co.jp>
2019-03-16 21:24:30 +0900

    userのAdd, List API追加

---
+++ user/create.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const uuid = require('uuid');
+const AWS = require('aws-sdk');
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+const s3 = new AWS.S3();
+
+module.exports.create = (event, context, callback) => {
+
+	const timestamp = new Date().getTime();
+	const data = JSON.parse(event.body);
+	
+	const params = {
+		TableName: process.env.DYNAMODB_TABLE_USER,
+		Item: {
+			userId: uuid.v1(),
+			userName: data.userName,
+			budget: data.budget,
+			userCategory: data.userCategory,
+			createdAt: timestamp,
+			updatedAt: timestamp,
+		},
+	};
+    
+    dynamodb.put(params, (error) => {
+        if (error) {
+            console.error(error);
+            callback(null, {
+                statusCode: error.statusCode || 501,
+                headers: { 'Content-Type': 'text/plain' },
+                body: 'Couldn\'t create the User.',
+            });
+            return;
+        }
+        
+        const response = {
+            statusCode: 200,
+            body: JSON.stringify(params.Item),
+        };
+        callback(null, response);
+    });
+};
